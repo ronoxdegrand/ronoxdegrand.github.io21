8350afae05796e4b21195c631e4c911740ddeb90
raghav <glgraghav@gmail.com>
2021-06-05 17:13:16 +0400

    clean navbar code

--- js/script1.js
+++ js/script1.js
@@ -1,16 +1,3 @@
-window.onscroll = function () {
-  myFunction();
-}
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 function toggle() {
   var x = document.getElementById("navbar");
   if (x.className === "topnav") {
